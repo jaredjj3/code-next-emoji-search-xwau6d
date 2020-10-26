f20d781b1585438c0904400052b7047fcc7d334a
jaredjj3 <jared@jaredjohnson.dev>
2020-10-26 17:02:47 -0400

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Header } from "./Header";
 import { Input } from "./Input";
 import { Results } from "./Results";
 
-
 export const App = () => {
   return (
     <div>
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React from "react";
+import "./Header.css";
+import { getSrc } from "./util/getSrc";
+
+const EMOJI1_SRC = getSrc("😈");
+const EMOJI2_SRC = getSrc("👿");
+
+const spongeBobCase = str => {
+  const chars = [];
+  for (let ndx = 0; ndx < str.length; ndx++) {
+    const char = str[ndx];
+    if (ndx % 2 === 0) {
+      chars.push(char.toUpperCase());
+    } else {
+      chars.push(char.toLowerCase());
+    }
+  }
+  return chars.join("");
+};
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header id="header">
+      <h1>
+        <img src={EMOJI1_SRC} width={32} height={32} />
+        {spongeBobCase('Emoji Search')}
+        <img src={EMOJI2_SRC} width={32} height={32} />
+      </h1>
+    </header>
+  );
 };
