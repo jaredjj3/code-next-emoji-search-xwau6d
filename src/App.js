4204b721f33928c9194ef4e7e5d44c28d1a9a0b3
jaredjj3 <jared@jaredjohnson.dev>
2020-10-26 16:48:07 -0400

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Header } from './Header';
+import { Header } from "./Header";
+import { Input } from "./Input";
+import { Results } from "./Results";
+
 
 export const App = () => {
   return (
     <div>
       <Header />
+      <Input />
+      <Results />
     </div>
   );
-}
+};
